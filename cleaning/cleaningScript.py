1db17925832779da04b219adf2cffd24acd011ce
Antonin SIMON <antonin.simon@epsi.fr>
2024-02-29 09:07:06 +0100

    Cleaning dataframe

---
+++ cleaning/cleaningScript.py
@@ -0,0 +1,19 @@
+import pandas as pd
+df_carb = pd.read_csv("../Prix_carburants_France_instantane.csv", delimiter=';', on_bad_lines='skip')
+# print(df_carb.head)
+
+#Affiche les colonnes avant nettoyage
+print("Colonnes originales : ", df_carb.columns)
+
+# Retire la colonne "pop" et "Services proposés" du dataframe (en erreur pour l'instant)
+df_carb.drop(columns=['pop', 'Services proposés' ], inplace=True)
+
+# Enlever les lignes qui n'ont pas d'informations dans "carburant disponible"
+df_carb.drop(df_carb[df_carb['Carburants disponibles'].notnull() == False].index, inplace = True)
+
+
+# Supprimer les lignes avec des valeurs manquantes dans la colonne "departement"
+df_carb = df_carb.dropna(subset=['Département'])
+
+#affiche les colonnes apres le nettoyage
+print("Colonnes après nettoyage : ", df_carb.columns)
